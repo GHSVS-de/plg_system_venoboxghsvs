ca7cbcf523278b542fb939eabbf543de23aee36a
GHSVS-de <contact-VolliSchlothauer@ghsvs.de>
2019-06-11 00:04:17 +0200

    Update venoboxghsvs.php

--- venoboxghsvs.php
+++ venoboxghsvs.php
@@ -1,14 +1,10 @@
 <?php
-/**
-2015-08-30: GHSVS
-*/
 defined('JPATH_BASE') or die;
+
 use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Plugin\CMSPlugin;
 
-// Bisschen deplaziert. Brauch ich für eigene Blog-View-Einstellungen.
-JFactory::getLanguage()->load('mod_j51news', JPATH_SITE . '/modules/mod_j51news');
-
-class PlgSystemvenoboxGhsvs extends JPlugin
+class PlgSystemvenoboxGhsvs extends CMSPlugin
 {
 	function __construct(&$subject, $config = array())
 	{
