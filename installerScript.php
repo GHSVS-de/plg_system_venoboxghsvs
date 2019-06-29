56ec7cd3021177783bb7007e17e5139beaf296d7
GHSVS-de <contact-VolliSchlothauer@ghsvs.de>
2019-06-29 14:10:15 +0200

    Correct copyright

--- installerScript.php
+++ installerScript.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * @package plugin.system hyphenateghsvs for Joomla!
- * @version See hyphenateghsvs.xml
+ * @package plugin.system venoboxghsvs for Joomla!
+ * @version See venoboxghsvs.xml
  * @author G@HService Berlin Neukölln, Volkmar Volli Schlothauer
  * @copyright Copyright (C) 2016-2019, G@HService Berlin Neukölln, Volkmar Volli Schlothauer. All rights reserved.
  * @license GNU General Public License version 3 or later; see LICENSE.txt; see also LICENSE_Hyphenator.txt; see also LICENSE_Hyphenopoly.txt
  * @authorUrl https://www.ghsvs.de
- * @link https://github.com/GHSVS-de/plg_system_hyphenateghsvs
+ * @link https://github.com/GHSVS-de/plg_system_venoboxghsvs
  */
 /**
  * Use in your extension manifest file (any tag is optional!!!!!):
