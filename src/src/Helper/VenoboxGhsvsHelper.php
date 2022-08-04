f4b3272f77473334b883973d0dc1ad3834d87bb9
GHSVS-de <contact-VolliSchlothauer@ghsvs.de>
2022-08-04 21:27:52 +0200

    VenoboxGhsvsHelper. Web asset manager if J!4

--- src/src/Helper/VenoboxGhsvsHelper.php
+++ src/src/Helper/VenoboxGhsvsHelper.php
@@ -15,36 +15,40 @@ abstract class VenoboxGhsvsHelper
 	protected static $loaded = [];
 	protected static $basepath = 'plg_system_venoboxghsvs/venobox';
 
-	/**
-	 * Lädt auch Params aus nicht aktivierten Plugins.
-   * Fügt den Params isEnabled- und isInstalled-Parameter hinzu.
-	*/
-	public static function getPluginParams(array $plugin)
+	protected static $plgParams;
+
+	protected static $isJ3 = true;
+
+	protected static $wa = null;
+
+	protected static function init()
 	{
-		if (count($plugin) != 2)
+		self::$isJ3 = version_compare(JVERSION, '4', 'lt');
+		self::$plgParams = self::getPluginParams();
+	}
+
+	/*
+	Ich habe nicht die leiseste Idee, warum ich diesen Kram verwenden muss(!), um
+	an die Plugin-params dran zu kommen. im Plugin selbst ist $this->params leer,
+	an der Stelle, wo ich das gerne abfragen würde.
+	Irgendeinen Grund wirds schon geben ;-)
+	A: Ich denke mal, weil der Einstiegspunkt der JHtml-Helfer ist, der ja auch
+	ohne aktives Plugin verwendet werden kann. Irgend so was...
+	*/
+	public static function getPluginParams($plugin = ['system', 'venoboxghsvs'])
+	{
+		if (empty(self::$plgParams) || !(self::$plgParams instanceof Registry))
 		{
-			return false;
-		}
+			self::$plgParams = PluginHelper::getPlugin($plugin[0], $plugin[1]);
 
-		$key = implode(':', $plugin);
-
-  	if(
-			empty(self::$loadedPlugins[$key])
-			|| !(self::$loadedPlugins[$key] instanceof Registry)
-		){
-			$pluginP = PluginHelper::getPlugin($plugin[0], $plugin[1]);
-
-			if (!empty($pluginP->params))
+			if (!empty(self::$plgParams->params))
 			{
-				self::$loadedPlugins[$key] = new Registry($pluginP->params);
-				self::$loadedPlugins[$key]->set('isEnabled', 1);
-				self::$loadedPlugins[$key]->set('isInstalled', 1);
+				self::$plgParams = new Registry(self::$plgParams->params);
+				self::$plgParams->set('isEnabled', 1);
+				self::$plgParams->set('isInstalled', 1);
 			}
-			// Falls deaktiviert, DB
-			elseif (\is_file(
-				JPATH_PLUGINS . '/' . implode('/', $plugin) . '/' . $plugin[1]
-					. '.php')
-			){
+			elseif (\is_file(JPATH_PLUGINS . '/' . implode('/', $plugin) . '/' . $plugin[1] . '.php'))
+			{
 				$db = Factory::getDbo();
 				$query = $db->getQuery(true)
 					->select('params')
@@ -53,31 +57,32 @@ abstract class VenoboxGhsvsHelper
 					->where('element = ' . $db->q($plugin[1]))
 					->where('folder = ' . $db->q($plugin[0]));
 				$db->setQuery($query);
-				$params = $db->loadResult();
+				self::$plgParams = $db->loadResult();
 
-				if (!empty($params))
+				if (!empty(self::$plgParams->params))
 				{
-					self::$loadedPlugins[$key] = new Registry($params);
-					self::$loadedPlugins[$key]->set('isEnabled', 0);
-					self::$loadedPlugins[$key]->set('isInstalled', 1);
+					self::$plgParams = new Registry(self::$plgParams->params);
+					self::$plgParams->set('isEnabled', 0);
+					self::$plgParams->set('isInstalled', 1);
 				}
 			}
-			// Falls erfolglos
-			else
+
+			if (empty(self::$plgParams))
 			{
-				self::$loadedPlugins[$key] = new Registry();
-				self::$loadedPlugins[$key]->set('isEnabled', -1);
-				self::$loadedPlugins[$key]->set('isInstalled', 0);
+				self::$plgParams = new Registry();
+				self::$plgParams->set('isEnabled', -1);
+				self::$plgParams->set('isInstalled', 0);
 			}
 		}
-		return self::$loadedPlugins[$key];
+
+		return self::$plgParams;
 	}
 
 	public static function prepareDefaultOptions(string $sig) : Array
 	{
 		if (!isset(static::$loaded[__METHOD__][$sig]))
 		{
-			$plgParams = self::getPluginParams(array('system', 'venoboxghsvs'));
+			$plgParams = self::getPluginParams();
 			$options_default = [];
 			$configs = $plgParams->get('configs');
 
@@ -142,15 +147,13 @@ abstract class VenoboxGhsvsHelper
 			}
 		}
 
-		$pluginParams = VenoboxGhsvsHelper::getPluginParams(
-			['system', 'venoboxghsvs']);
+		$pluginParams = self::getPluginParams();
 
 		if (!isset($options['selector']))
 		{
 			if (!($options['selector'] = $selector))
 			{
-				$selector = $pluginParams->get('selector', '');
-				$selector = trim($selector);
+				$selector = trim($pluginParams->get('selector', ''));
 				$options['selector'] = $selector ?: '.venobox';
 			}
 		}
@@ -163,42 +166,76 @@ abstract class VenoboxGhsvsHelper
 			return;
 		}
 
+		$wa = self::getWa();
+
 		if (!isset(static::$loaded[__METHOD__]['core']))
 		{
-			$min = JDEBUG ? '' : '.min';
-			$version = JDEBUG ? time() : 'auto';
+			if ($wa)
+			{
+				$wa->usePreset('plg_system_venoboxghsvs.all');
+			}
+			else
+			{
+				$min = JDEBUG ? '' : '.min';
+				$version = JDEBUG ? time() : 'auto';
 
-			HTMLHelper::_('stylesheet',
-				static::$basepath . '/' . 'venobox' . $min . '.css',
-				['version' => $version, 'relative' => true]
-			);
+				HTMLHelper::_('stylesheet',
+					static::$basepath . '/' . 'venobox' . $min . '.css',
+					['version' => $version, 'relative' => true]
+				);
 
-			// e.g. compiled from SCSS
-			$customCSSPath = 'templates/'
-				. Factory::getApplication()->getTemplate()
-				. '/css/venobox' . $min . '.css';
+				// e.g. compiled from SCSS
+				$customCSSPath = 'templates/'
+					. Factory::getApplication()->getTemplate()
+					. '/css/venobox' . $min . '.css';
 
-			HTMLHelper::_('stylesheet',
-				$customCSSPath,
-				['version' => $version]
-			);
-			HTMLHelper::_('script',
-				static::$basepath . '/' . 'venobox' . $min . '.js',
-				['version' => $version, 'relative' => true]
-			);
+				HTMLHelper::_('stylesheet',
+					$customCSSPath,
+					['version' => $version]
+				);
 
+				HTMLHelper::_('script',
+					static::$basepath . '/' . 'venobox' . $min . '.js',
+					['version' => $version, 'relative' => true]
+				);
+			}
 			static::$loaded[__METHOD__]['core'] = 1;
 		}
 
 		$options = \array_merge(
-			VenoboxGhsvsHelper::prepareDefaultOptions($sig),
+			self::prepareDefaultOptions($sig),
 			$options
 		);
 
 		$js = 'document.addEventListener("DOMContentLoaded", (event) => {'
 			. 'new VenoBox(' . json_encode($options) . ')});';
 
-		Factory::getDocument()->addScriptDeclaration($js);
+		if ($wa)
+		{
+			$wa->addInline(
+				'script',
+				$js,
+				['name' => 'plg_system_venoboxghsvs.' . 'selector-' . str_replace(' ', '', $sig)]
+			);
+		}
+		else
+		{
+			Factory::getDocument()->addScriptDeclaration($js);
+		}
+
 		static::$loaded[__METHOD__][$sig] = 1;
+	}
+
+	public static function getWa()
+	{
+		self::init();
+
+		if (self::$isJ3 === false && empty(self::$wa))
+		{
+			self::$wa = Factory::getApplication()->getDocument()->getWebAssetManager();
+			self::$wa->getRegistry()->addExtensionRegistryFile('plg_system_venoboxghsvs');
+		}
+
+		return self::$wa;
 	}
 }
